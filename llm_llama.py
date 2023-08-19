58241cb8115397efc7702986275d461731acd0c2
raftaar bhai <bhai@gmail.com>
2023-08-19 08:52:09 +0200

    chore: add prompt weighting

--- llm_llama.py
+++ llm_llama.py
@@ -2,6 +2,7 @@ import llm
 import random
 import torch
 import transformers
+import re
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from pydantic import field_validator, Field
 from typing import Optional
@@ -46,18 +47,19 @@ class Llama(llm.Model):
 
 
     def execute(self, prompt, stream, response, conversation):
+       model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+       tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
        if prompt.options.technique == "alternating":
-           return self.execute_prompt_alternating(prompt.prompt)
+           return self.execute_prompt_alternating(prompt.prompt, model, tokenizer)
        elif prompt.options.technique == "prompt-weighting":
-           return self.execute_prompt_weighting()
+           return self.execute_prompt_weighting(prompt.prompt, model, tokenizer)
       
 
 
 
 
-    def execute_prompt_alternating(self, prompt: str):
-       model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
-       tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+    def execute_prompt_alternating(self, prompt: str, model, tokenizer):
+
        num_tokens = 20
        prompt_tokens = tokenizer.encode(prompt, return_tensors="pt")
        output_tokens = prompt_tokens.clone()
@@ -73,10 +75,38 @@ class Llama(llm.Model):
            next_token = model.generate(input_ids, max_length=input_ids.shape[1] + 1, do_sample = True)[:, -1].unsqueeze(0)
 
            output_tokens = torch.cat((output_tokens, next_token), dim=-1)
-       print("success")
        generated_text = tokenizer.decode(output_tokens[0], skip_special_tokens=True)
        return generated_text
 
-    def execute_prompt_weighting(self):
-        return "weighting"
-        raise NotImplementedError("not implemented")
+    def execute_prompt_weighting(self, prompt, model, tokenizer, **kwargs):
+        def modify_attention_mask(prompt, model, tokenizer):
+            print("starting attention mask")
+            tokens = []
+            attention_modifiers = []
+            add_space = False
+            for token in re.split(r'\(|\)', prompt):
+                print(f"current token is {token}")
+                if ':' in token:
+                    word, modifier = token.split(':')
+                    modifier = float(modifier.strip())
+                else:
+                    word = token.strip()
+                    modifier = 1.0
+                current_tokens = tokenizer.tokenize(word)
+                if add_space and current_tokens:
+                    tokens.append('Ġ')  # Space token for GPT-2
+                    attention_modifiers.append(1.0)
+                tokens.extend(current_tokens)
+                attention_modifiers.extend([modifier] * len(current_tokens))
+                add_space = True
+            attention_mask = torch.tensor([attention_modifiers])
+            input_ids = torch.tensor([tokenizer.convert_tokens_to_ids(tokens)])
+            return input_ids, attention_mask
+        input_ids, attention_mask = modify_attention_mask(prompt, model, tokenizer)
+        print(attention_mask)
+        # Set the modified attention mask
+        model.config.attention_probs_dropout_prob = 0.0
+        with torch.no_grad():
+            output_sequences = model.generate(input_ids=input_ids, attention_mask=attention_mask, **kwargs)
+            print(output_sequences)
+        return tokenizer.decode(output_sequences[0], skip_special_tokens=True)
